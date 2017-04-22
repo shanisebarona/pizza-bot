5e80d87a00430b27f548ea97b08f159adc34c57f
Shanise <shanisebarona@gmail.com>
2017-04-22 12:09:07 -0400

    steps zero through two on pizzabot

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const pizzapi = require('dominos')
+
 require('skellington')({
   slackToken: process.env.SLACK_TOKEN,
-  plugins: [require('./plugins/welcome')]
+  plugins: [require('./plugins/welcome'), require('./plugins/order')]
 })
